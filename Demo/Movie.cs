77ae3993be7bdc6c153e45f3f115b7275f635cb1
Vijay Nathani <vijay_nathani@yahoo.com>
2024-08-05 14:05:35 +0530

    Refactor

---
+++ Demo/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace demoR;
+
+internal class Movie
+{
+    public const int CHILDRENS = 2, REGULAR = 0, NEW_RELEASE = 1;
+    private readonly string _title;
+
+    public Movie(string title, int priceCode)
+    {
+        _title = title;
+        PriceCode = priceCode;
+    }
+
+    public string Title
+    {
+        get { return _title; }
+    }
+
+    public int PriceCode { get; set; }
+}
---
+++ Demo/Rental.cs
@@ -0,0 +1,23 @@
+﻿namespace demoR;
+
+internal class Rental
+{
+    private readonly int _daysRented;
+    private readonly Movie _movie;
+
+    public Rental(Movie movie, int daysRented)
+    {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public Movie Movie
+    {
+        get { return _movie; }
+    }
+
+    public int DaysRented
+    {
+        get { return _daysRented; }
+    }
+}

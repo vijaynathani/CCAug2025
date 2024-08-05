ed18cd4c74fe0102da6ce75f80163951ce8c05f8
Vijay Nathani <vijay_nathani@yahoo.com>
2024-08-05 10:29:56 +0530

    Update S022.cs

--- Code/S022.cs
+++ Code/S022.cs
@@ -1,27 +1,18 @@
 using System;
-//Improve code
+class TelNo {
+    string countryCode, areaCode, localNumber;
+}
+class Name {
+    string first, last;
+}
+class Person {
+    Name english, chinese;
+    TelNo telephone, fax, mobile;
+}
 class Organization {
     string id;
-    string eName; //English name
-    string cName; //Chinese name
-    string telCountryCode;
-    string telAreaCode;
-    string telLocalNumber;
-    string faxCountryCode;
-    string faxAreaCode;
-    string faxLocalNumber;
-    string contactPersonEFirstName; //First name and last name in English
-    string contactPersonELastName;
-    string contactPersonCFirstName; //First name and last name in Chinese
-    string contactPersonCLastName;
-    string contactPersonTelCountryCode;
-    string contactPersonTelAreaCode;
-    string contactPersonTelNumber;
-    string contactPersonFaxCountryCode;
-    string contactPersonFaxAreaCode;
-    string contactPersonFaxLocalNumber;
-    string contactPersonMobileCountryCode;
-    string contactPersonMobileAreaCode;
-    string contactPersonMobileLocalNumber;
+    string englishName, chineseName;
+    TelNo mainLineNumber, faxNo;
+    Person contact;
     //...
 }

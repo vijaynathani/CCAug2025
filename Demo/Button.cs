5f5ff243ec3c7bee333d54465e3d04505dc14e15
Vijay Nathani <vijay_nathani@yahoo.com>
2024-08-05 14:50:35 +0530

    Create Button.cs

---
+++ Demo/Button.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    interface ButtonServer
+    {
+        void turnOn();
+        void turnOff();
+    }
+    internal class Button
+    {
+        ButtonServer device;
+        public Button(ButtonServer device)
+        {
+            this.device = device;
+        }
+    }
+    internal class Lamp : ButtonServer
+    {
+        public void turnOff()
+        {
+        }
+
+        public void turnOn()
+        {
+        }
+    }
+}
